6236e5e33cd71b469edc5d926b617d925da53683
Mohammad Haizad <26133523+haizadtarik@users.noreply.github.com>
2019-05-11 10:52:33 +0800

    Update optimize.m

--- optimize.m
+++ optimize.m
@@ -26,39 +26,6 @@ SearchAgents = nvars*10;
 options =   optimoptions('particleswarm', 'MaxIterations',iteration, 'display','iter','SwarmSize',SearchAgents); 
 [weights_optimize, cost] = particleswarm(costFunction ,nvars,lb,ub,options);
 
-% =================== SA ==================
-% alpha = 0.8;
-% [weights_optimize,cost]=sa_con(costFunction,nvars,lb,ub,iteration,alpha);
-
-% =================== FPA ==================
-% probability =0.2;
-% [weights_optimize, cost]=FPA(costFunction ,nvars,lb,ub,SearchAgents,probability,iteration);
-
-% =================== BAT ==================
-% Loudness = 0.7;
-% pulse_rate = 0.7;
-% [weights_optimize, cost]=bat_algorithm(costFunction,nvars,lb,ub,SearchAgents,iteration,Loudness,pulse_rate);
-
-% =================== FFA ==================
-%[weights_optimize,cost]=ffa_con(costFunction,nvars,lb,ub,SearchAgents,iteration);
-
-% =================== Cuckoo ==================
-% [weights_optimize,cost] = cuckoo_search(costFunction,nvars,lb,ub,SearchAgents,iteration);
-
-
-% =================== SSA ==================
-% addpath 'SSA';
-% [cost,weights_optimize,~]=SSA(SearchAgents,iteration,lb,ub,nvars,costFunction);
-
-% =================== WOA ==================
-% addpath 'WOA';
-% [cost,weights_optimize,~]=WOA(SearchAgents,iteration,lb,ub,nvars,costFunction);
-
-% =================== ALO ==================
-% addpath 'ALO';
-% [cost,weights_optimize,~]=ALO(SearchAgents,iteration,lb,ub,nvars,costFunction);
-
-
 % ----------------------- Obtain Theta1, Theta2 and bias back from weights_optimize -------------------------------------
 Theta1 = reshape(weights_optimize(1:hidden_unit*input_layer_size), hidden_unit, input_layer_size);
 Theta2 = reshape(weights_optimize(1+(hidden_unit*input_layer_size):(hidden_unit*input_layer_size)+(hidden_unit*output_layer_size)), output_layer_size, hidden_unit);      
